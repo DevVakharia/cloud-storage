edaa27bf6d9fbe8cca33cfe01010f58e1584fd0e
DevVakharia <devvakharia@gmail.com>
2025-07-25 19:05:33 +0530

    Fix case sensitivity in filenames

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
-import './App.css';
+import App from './app';
+import './app.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
